a1b8474fe706383ef984d3514408e8f72951097c
unknown <jamalnasirone52@gmail.com>
2025-12-15 14:31:46 +0000

    ts update

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,13 +1,13 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
-import UserNameMenu from "./userNameMenu";
+import UserNameMenu from "./UserNameMenu";
 
 const MainNav = () => {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
   return (
     <span className="flex space-x-2 items-center">
       {isAuthenticated ? (
-       <UserNameMenu />
+        <UserNameMenu />
       ) : (
         <Button
           variant={"ghost"}
